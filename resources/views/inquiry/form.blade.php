ade1f5865a801e31c4c2150c08143295bc87924c
seiis <seiisland@gmail.com>
2025-02-11 21:18:28 +0900

    HTMLタイトル整備

--- resources/views/inquiry/form.blade.php
+++ resources/views/inquiry/form.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>お問い合わせフォーム</title>
+    <title>TaxBar&regお問合わせフォーム</title>
     @vite(['resources/css/app.css'])
 </head>
 
--- resources/views/taxminivideos/index.blade.php
+++ resources/views/taxminivideos/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TaxMiniVideos</title>
+    <title>TaxBar&regTaxMinutes&reg</title>
     <link rel="stylesheet" href="{{ asset('css/taxministyle.css') }}">
 </head>
 
