9d2845aa6b754d73605a96cd09acc5dcf9b2fa24
seiis <seiisland@gmail.com>
2025-02-12 18:28:56 +0900

    問合せフォーム背景を白に

--- resources/views/inquiry/form.blade.php
+++ resources/views/inquiry/form.blade.php
@@ -18,10 +18,10 @@
         </div>
         <nav class="nav">
             <ul>
+                <li><a href="/">HOME</a></li>
                 <li><a href="/taxminivideos">Tax Minutes&reg</a></li>
                 <li><a href="#TaxBarabout">テーマ</a></li>
                 <li><a href="/view/prohibited">禁止事項</a></li>
-                <li><a href="/inquiry">問合せ</a></li>
                 <li><a href="/view/hachimantaishi">八幡平市</a></li>
                 <li><a href="/faq">よくある質問</a></li>
                 <li><a href="/pricing">料金表</a></li>
