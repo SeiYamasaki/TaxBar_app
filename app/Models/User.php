fb64d1caca723be1b0694eac583bb75b267143fc
zonehisa <nijipapazoneokun123@gmail.com>
2025-03-04 09:07:53 +0900

    web.php,maigation

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,9 +14,28 @@ class User extends Authenticatable
         'email',
         'password',
         'is_tax_accountant', // 追加
+        'office_name',
+        'postal_code',
+        'prefecture',
+        'address',
+        'office_phone',
+        'mobile_phone',
+        'tax_registration_number',
+        'plan',
+        'tax_accountant_photo',
+        'additional_photos',
     ];
 
     protected $casts = [
         'is_tax_accountant' => 'boolean', // 型を boolean にキャスト
+        'additional_photos' => 'array',
     ];
+
+    /**
+     * ユーザーが投稿したTaxMinutesビデオを取得
+     */
+    public function taxMinutesVideos()
+    {
+        return $this->hasMany(TaxMinutesVideo::class);
+    }
 }
